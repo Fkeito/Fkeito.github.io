eb673e8fb14b129b76db0d793dccbdf347c70b07
Fkeito <kei1229tomi@gmail.com>
2019-07-08 14:39:30 +0900

    onanimationend

--- Top/anim.js
+++ Top/anim.js
@@ -3,9 +3,15 @@ function translateBox(b){
 }
 function moveBox(b){
     b.classList.add('move');
-    b.onanimationend=function(){b.classList.remove('move')};
+    //b.onanimationend=function(){b.classList.remove('move')};
+    b.addEventListener('animationend',()=>{
+        b.classList.remove('move')
+    })
 }
 function rotateBox(b){
     b.classList.add('rotate');
-    b.onanimationend=function(){b.classList.remove('rotate')};
+    //b.onanimationend=function(){b.classList.remove('rotate')};
+    b.addEventListener('animationend',()=>{
+        b.classList.remove('rotate')
+    })
 }
