526689649ae3d0ba2ff65d807a76314191aec823
Fkeito <kei1229tomi@gmail.com>
2019-07-10 01:27:24 +0900

    なんかかわいいのできた

--- ThreeTest/index.js
+++ ThreeTest/index.js
@@ -9,7 +9,7 @@ function init(){
     document.body.appendChild(renderer.domElement);
 
     mainCamera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
-    mainCamera.position.set(0, 0, 10);
+    mainCamera.position.set(0, 0, 20);
 
     scene = new THREE.Scene();
 
@@ -27,25 +27,57 @@ let renderer;
 let scene;
 let mainCamera;
 
-let box;
+let boxes;
+let spheres = [];
 
 let wheelDelta = 0;
 let mouseDragXDelta = 0;
 let distance = 10;
 let deg = 0;
+const materials = [
+    new THREE.MeshBasicMaterial({color: 0xff6699}),
+    new THREE.MeshBasicMaterial({color: 0x99ff66}),
+    new THREE.MeshBasicMaterial({color: 0x6699ff}),
+    new THREE.MeshBasicMaterial({color: 0xffffff}),
+    new THREE.MeshBasicMaterial({color: 0x000000}),
+];
 
 function awake(){
-    box = Box(1,1,1);
-    scene.add(box);
-
+    /*
     renderer.domElement.onwheel = onMouseWheel;
     renderer.domElement.onmouseup = onMouseClickUp;
     renderer.domElement.onmousedown = onMouseClickDown;
     renderer.domElement.onmousemove = onMouseMove;
+    */
+    renderer.domElement.onmousedown = function(){
+        if(isChanging) return;
+        isChanging = true;
+        change = setInterval(changeColor, 1000);
+    }
 
     requestAnimationFrame(start);
 }
 function start(){
+    boxes = [
+        Box(1, 1, 1, materials[3]),
+        Box(0.6, 0.6, 0.6, materials[0]),
+        Box(1, 1, 1, materials[3]),
+        Box(0.6, 0.6, 0.6, materials[1]),
+        Box(1, 1, 1, materials[3]),
+        Box(0.6, 0.6, 0.6, materials[2])
+    ];
+    for(let i = 0;i < 6;i++){
+        boxes[i].position.set((2 - 0.1 * (i % 2)) * (Math.floor(i / 2) - 1), 0, i % 2);
+        scene.add(boxes[i]);
+    }
+    for(let i = -1;i <= 1;i += 2){
+        for(let j = -1;j <= 1;j++){
+            const sphere = Sphere(0.1, 4, 4, materials[3]);
+            sphere.position.x = 0.75 * j + 5 * i;
+            spheres.push(sphere);
+            scene.add(sphere);
+        }
+    }
 
     update();
 }
@@ -53,29 +85,61 @@ function update(){
     requestAnimationFrame(update);
 
     //camera
+    /*
     distance = Math.min(20, Math.max(5, distance + wheelDelta * 0.05));
     wheelDelta = 0;
-    deg += mouseDragXDelta;
+    deg = Math.min(4, Math.max(-4, deg + mouseDragXDelta * 0.05));
     mainCamera.position.x = distance * Math.sin(deg * 0.01);
     mainCamera.position.z = distance * Math.cos(deg * 0.01);
+    */
 
     mainCamera.lookAt(new THREE.Vector3(0, 0, 0));
 
-    box.rotation.x += 0.01;
-    box.rotation.y += 0.01;
+    for(let i = 0;i < 6;i++){
+        boxes[i].rotation.x += 0.01;
+        boxes[i].rotation.y += 0.01;
+    }
 
     renderer.render(scene, mainCamera);
 }
 
-function Box(x,y,z){
+function Box(x,y,z,material){
     const geometory = new THREE.BoxGeometry(x, y, z);
-    const material = new THREE.MeshStandardMaterial({
-        color: 0xffffff
-    });
+    const box = new THREE.Mesh(geometory, material);
+    return box;
+}
+function Sphere(r,w,h,material){
+    const geometory = new THREE.SphereGeometry(r, w, h);
     const box = new THREE.Mesh(geometory, material);
     return box;
 }
 
+let change;
+let isChanging = false;
+let colorIndex = 0;
+function changeColor(e){
+    if(colorIndex > 2){
+        for(let i = 0;i < 3;i++){
+            boxes[i * 2 + 1].material = materials[i];
+            spheres[i].material = materials[3];
+            spheres[i + 3].material = materials[3];
+        }
+
+        isChanging = false;
+        colorIndex = 0;
+        clearInterval(change);
+        return;
+    }
+
+    boxes[colorIndex * 2 + 1].material = materials[4];
+    spheres[colorIndex].material = materials[colorIndex];
+    spheres[colorIndex + 3].material = materials[colorIndex];
+
+    colorIndex++;
+}
+
+
+/*
 function onMouseWheel(e){
     wheelDelta = e.deltaY;
 }
@@ -95,4 +159,4 @@ function onMouseMove(e){
         preMousePos = e.clientX;
     }
 }
-
+*/
