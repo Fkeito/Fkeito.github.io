a1cf3418464ee5e618b76b50c8972411175cfa6d
Fkeito <kei1229tomi@gmail.com>
2019-07-09 21:50:21 +0900

    カメラ動かせる

--- ThreeTest/index.js
+++ ThreeTest/index.js
@@ -1,11 +1,4 @@
 window.addEventListener('DOMContentLoaded', init);
-class Scene{
-    constructor(camera){
-        this.scene = new THREE.Scene();
-
-        this.mainCamera = camera;
-    }
-}
 function init(){
     const width = window.innerWidth;
     const height = window.innerHeight;
@@ -15,33 +8,40 @@ function init(){
     renderer.setSize(width, height);
     document.body.appendChild(renderer.domElement);
 
-    const cam = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
-    cam.position.set(0, 0, 10);
+    mainCamera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
+    mainCamera.position.set(0, 0, 10);
 
-    mainScene = new Scene(cam);
+    scene = new THREE.Scene();
 
     const dirLight = new THREE.DirectionalLight(0xffffff);
     dirLight.intensity = 2;
     dirLight.position.set(1, 1, 1);
-    mainScene.scene.add(dirLight);
+    scene.add(dirLight);
 
-    renderer.render(mainScene.scene, mainScene.mainCamera);
-
-    renderer.domElement.onwheel = onMouseWheel;
+    renderer.render(scene, mainCamera);
 
     requestAnimationFrame(awake);
 }
 
 let renderer;
-let mainScene;
+let scene;
+let mainCamera;
 
 let box;
 
 let wheelDelta = 0;
+let mouseDragXDelta = 0;
+let distance = 10;
+let deg = 0;
 
 function awake(){
     box = Box(1,1,1);
-    mainScene.scene.add(box);
+    scene.add(box);
+
+    renderer.domElement.onwheel = onMouseWheel;
+    renderer.domElement.onmouseup = onMouseClickUp;
+    renderer.domElement.onmousedown = onMouseClickDown;
+    renderer.domElement.onmousemove = onMouseMove;
 
     requestAnimationFrame(start);
 }
@@ -52,13 +52,19 @@ function start(){
 function update(){
     requestAnimationFrame(update);
 
-    mainScene.mainCamera.position.z = Math.min(20, Math.max(5, mainScene.mainCamera.position.z + wheelDelta * 0.05));
+    //camera
+    distance = Math.min(20, Math.max(5, distance + wheelDelta * 0.05));
     wheelDelta = 0;
+    deg += mouseDragXDelta;
+    mainCamera.position.x = distance * Math.sin(deg * 0.01);
+    mainCamera.position.z = distance * Math.cos(deg * 0.01);
+
+    mainCamera.lookAt(new THREE.Vector3(0, 0, 0));
 
     box.rotation.x += 0.01;
     box.rotation.y += 0.01;
 
-    renderer.render(mainScene.scene, mainScene.mainCamera);
+    renderer.render(scene, mainCamera);
 }
 
 function Box(x,y,z){
@@ -72,5 +78,21 @@ function Box(x,y,z){
 
 function onMouseWheel(e){
     wheelDelta = e.deltaY;
+}
+let isDrag = false;
+let preMousePos;
+function onMouseClickDown(e){
+    isDrag = true;
+    preMousePos = e.clientX;
+}
+function onMouseClickUp(e){
+    isDrag = false;
+    mouseDragXDelta = 0;
+}
+function onMouseMove(e){
+    if(isDrag){
+        mouseDragXDelta = preMousePos - e.clientX;
+        preMousePos = e.clientX;
+    }
 }
 
